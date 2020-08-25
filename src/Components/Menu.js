da97c10ef0e139863e4ef5486e60abcd5843bbc0
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-08-25 11:47:39 +0300

    menuStarted

---
+++ src/Components/Menu.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import dbMenu from './DBMenu';
+import { ListItem } from './ListItem'
+import bannerImage from '../Image/banner.png'
+
+const MenuStyled = styled.main`
+    background-color: #ccc;
+    margin-top: 80px;
+`;
+
+const SectionMenu = styled.section`
+    padding: 30px;
+`;
+
+const BannerMenu = styled.img`
+    height: 210px;
+    width: 100%;
+`;
+
+export const Menu = () => (
+    <MenuStyled>
+        <BannerMenu src={bannerImage} alt="banner"/>
+        <SectionMenu>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger}/>
+        </SectionMenu>
+        <SectionMenu>
+            <h2>Закуски / напитки</h2>
+            <ListItem itemList={dbMenu.other}/>
+        </SectionMenu>
+    </MenuStyled>
+);
